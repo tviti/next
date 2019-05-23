e8855be4dcd9ddadfe9f996a3c062aa3686ccd7a
Pierre Neidhardt <mail@ambrevar.xyz>
2019-05-23 12:13:07 +0200

    FIX port move xml

--- ports/gtk-webkit/server.h
+++ ports/gtk-webkit/server.h
@@ -472,76 +472,11 @@ static const GDBusInterfaceVTable interface_vtable = {
 	NULL
 };
 
-/* Introspection data for the service we are exporting */
-static const gchar introspection_xml[] =
-	"<node>"
-	"  <interface name='engineer.atlas.next.platform'>"
-	"    <method name='window_make'>"
-	"      <arg type='s' name='id' direction='in'/>"
-	"      <arg type='s' name='new_id' direction='out'/>"
-	"    </method>"
-	"  </interface>"
-	"  <interface name='engineer.atlas.next.platform'>"
-	"    <method name='window_set_title'>"
-	"      <arg type='s' name='id' direction='in'/>"
-	"      <arg type='s' name='title' direction='in'/>"
-	"      <arg type='b' name='status' direction='out'/>"
-	"    </method>"
-	"  </interface>"
-	"  <interface name='engineer.atlas.next.platform'>"
-	"    <method name='window_active'>"
-	"      <arg type='s' name='id' direction='out'/>"
-	"    </method>"
-	"  </interface>"
-	"  <interface name='engineer.atlas.next.platform'>"
-	"    <method name='window_set_active_buffer'>"
-	"      <arg type='s' name='window_id' direction='in'/>"
-	"      <arg type='s' name='buffer_id' direction='in'/>"
-	"      <arg type='b' name='status' direction='out'/>"
-	"    </method>"
-	"  </interface>"
-	"  <interface name='engineer.atlas.next.platform'>"
-	"    <method name='window_set_minibuffer_height'>"
-	"      <arg type='s' name='window_id' direction='in'/>"
-	"      <arg type='i' name='height' direction='in'/>"
-	"      <arg type='i' name='new_height' direction='out'/>"
-	"    </method>"
-	"  </interface>"
-	"  <interface name='engineer.atlas.next.platform'>"
-	"    <method name='buffer_make'>"
-	"      <arg type='s' name='id' direction='in'/>"
-	"      <arg type='a' name='options' direction='in'/>"
-	"      <arg type='b' name='new_id' direction='out'/>"
-	"    </method>"
-	"  </interface>"
-	"  <interface name='engineer.atlas.next.platform'>"
-	"    <method name='buffer_load'>"
-	"      <arg type='s' name='buffer_id' direction='in'/>"
-	"      <arg type='s' name='uri' direction='in'/>"
-	"      <arg type='b' name='status' direction='out'/>"
-	"    </method>"
-	"  </interface>"
-	"  <interface name='engineer.atlas.next.platform'>"
-	"    <method name='buffer_evaluate_javascript'>"
-	"      <arg type='s' name='buffer_id' direction='in'/>"
-	"      <arg type='s' name='javascript' direction='in'/>"
-	"      <arg type='s' name='callback_id' direction='out'/>"
-	"    </method>"
-	"  </interface>"
-	"  <interface name='engineer.atlas.next.platform'>"
-	"    <method name='minibuffer_evaluate_javascript'>"
-	"      <arg type='s' name='window_id' direction='in'/>"
-	"      <arg type='s' name='javascript' direction='in'/>"
-	"      <arg type='s' name='callback_id' direction='out'/>"
-	"    </method>"
-	"  </interface>"
-	"</node>";
-
 void start_server(GDBusConnection *connection,
 	const gchar *name,
 	gpointer user_data) {
 	GDBusNodeInfo *introspection_data =
-		g_dbus_node_info_new_for_xml(introspection_xml, NULL);
+		g_dbus_node_info_new_for_xml(state.introspection_xml, NULL);
 
 	guint registration_id = g_dbus_connection_register_object(connection,
 			PLATFORM_PORT_OBJECT,
