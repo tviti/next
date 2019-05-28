701745561cc8d7becf2cd4ea717bdef812b353bc
Pierre Neidhardt <mail@ambrevar.xyz>
2019-05-28 16:53:42 +0200

    dbus: Delete toy examples

--- dbus/cl.lisp
+++
@@ -1,8 +0,0 @@
-(with-open-bus (bus (session-server-addresses))
-  (with-introspected-object (notifications bus "/org/gtk/GDBus/TestObject" "org.gtk.GDBus.TestServer")
-    (notifications "org.gtk.GDBus.TestInterface" "HelloWorld" "foo")))
-
-(defvar *interfaces* (dbus:list-object-interfaces (with-open-bus (bus (session-server-addresses))
-									 (make-object-from-introspection (bus-connection bus)  "/org/gtk/GDBus/TestObject" "org.gtk.GDBus.TestServer"))))
-
-(dbus:list-interface-methods (first *interfaces*))
