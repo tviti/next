atlas-engineer/nyxt

Use minibuffer as argument to read-from-minibuffer.

source/file-manager-mode.lisp
@@ -143,9 +143,10 @@ Note: this feature is alpha, get in touch for more!"
     ;; new minibuffer instance.
     (push mode (modes (minibuffer *interface*)))
     (with-result (filename (read-from-minibuffer
-                            :input-prompt (file-namestring directory)
-                            :completion-function #'next/file-manager-mode::open-file-from-directory-completion-fn
-                            :cleanup-function #'next/file-manager-mode::clean-up-open-file-mode))
+                            (make-instance 'minibuffer
+                                           :input-prompt (file-namestring directory)
+                                           :completion-function #'next/file-manager-mode::open-file-from-directory-completion-fn
+                                           :cleanup-function #'next/file-manager-mode::clean-up-open-file-mode)))
 
       (funcall next/file-manager-mode::*open-file-function* (namestring filename)))))
 

tviti/next

source/file-manager-mode.lisp
@@ -139,9 +139,10 @@ Note: this feature is alpha, get in touch for more!"
     ;; new minibuffer instance.
     (push mode (modes (minibuffer *interface*)))
     (with-result (filename (read-from-minibuffer
-                            :input-prompt (file-namestring directory)
-                            :completion-function #'next/file-manager-mode::open-file-from-directory-completion-fn
-                            :cleanup-function #'next/file-manager-mode::clean-up-file-manager-mode))
+                            (make-instance 'minibuffer
+                                           :input-prompt (file-namestring directory)
+                                           :completion-function #'next/file-manager-mode::open-file-from-directory-completion-fn
+                                           :cleanup-function #'next/file-manager-mode::clean-up-file-manager-mode)))
 
       (funcall next/file-manager-mode::*open-file-function* (namestring filename)))))
 